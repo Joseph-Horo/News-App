3341b1c842b275f07a39541077a7fd4a13a04afd
Joseph-Horo <horojoseph19@gmail.com>
2024-08-12 11:34:50 +0300

    News App

---
+++ app/src/main/java/com/example/thenewsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.thenewsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "449cb595ccf642b39651405e9a03c6d4"
+        const val BASE_URL = "https://newsapi.org/"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+    }
+}
